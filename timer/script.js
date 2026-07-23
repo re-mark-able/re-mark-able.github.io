f3f5ac7f8e43bac36b6a55866bb00233fc63f5fd
re-mark-able <markbmaximus@gmail.com>
2026-07-23 19:36:46 +0800

    fixes

--- timer/script.js
+++ timer/script.js
@@ -1,23 +1,51 @@
 var timeSet = false;
+var endTime = new Date();
 
-function getEndTime() {
-  const end = new Date();
-  end.setSeconds(
-    end.getSeconds() + parseInt(document.getElementById("secondsInput").value),
+// Change end time when needed (buttons pressed or on keyup)
+function setEndTimeValue() {
+  endTime.setSeconds(
+    endTime.getSeconds() +
+      parseInt(document.getElementById("secondsInput").value),
   );
-  end.setMinutes(
-    end.getMinutes() + parseInt(document.getElementById("minutesInput").value),
+  endTime.setMinutes(
+    endTime.getMinutes() +
+      parseInt(document.getElementById("minutesInput").value),
   );
-  end.setHours(
-    end.getHours() + parseInt(document.getElementById("hoursInput").value),
+  endTime.setHours(
+    endTime.getHours() + parseInt(document.getElementById("hoursInput").value),
   );
 
-  return end;
+  // Start hasn't been pressed, update the end
+  if (!timeSet) {
+    document.getElementById("endTime").innerHTML = endTime.toLocaleTimeString();
+  }
+}
+
+// + / - button presses
+function handleHourClick(add = true) {
+  const currentValue = (document.getElementById("hoursInput").value = !add
+    ? parseInt(document.getElementById("hoursInput").value) - 1
+    : parseInt(document.getElementById("hoursInput").value) + 1);
+
+  setEndTimeValue();
+}
+
+function handleMinuteClick(add = true) {
+  const currentValue = (document.getElementById("minutesInput").value = !add
+    ? parseInt(document.getElementById("minutesInput").value) - 1
+    : parseInt(document.getElementById("minutesInput").value) + 1);
+  setEndTimeValue();
+}
+
+function handleSecondClick(add = true) {
+  const currentValue = (document.getElementById("secondsInput").value = !add
+    ? parseInt(document.getElementById("secondsInput").value) - 1
+    : parseInt(document.getElementById("secondsInput").value) + 1);
+  setEndTimeValue();
 }
 
 function timeSince() {
-  const end = getEndTime();
-  const total = Date.parse(end) - Date.parse(new Date());
+  const total = Date.parse(endTime) - Date.parse(new Date());
   const seconds = Math.floor((total / 1000) % 60);
   const minutes = Math.floor((total / 1000 / 60) % 60);
   const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
@@ -26,57 +54,25 @@ function timeSince() {
   return `${hours < 10 ? "0" + hours : hours}:${minutes < 10 ? "0" + minutes : minutes}:${seconds < 10 ? "0" + seconds : seconds}`;
 }
 
-function setEndTime() {
-  const end = getEndTime();
-  document.getElementById("endTime").innerHTML = end.toLocaleTimeString();
-  setRemainingTime();
-}
-
 function setRemainingTime() {
   const now = new Date();
-  const end = getEndTime();
 
-  const diff = end.getTime() - now.getTime();
+  const diff = endTime.getTime() - now.getTime();
 
   if (diff < 0) {
     document.getElementById("remainingTime").innerHTML = "Time's up!";
   } else {
     document.getElementById("remainingTime").innerHTML = timeSince(
-      end,
+      endTime,
     ).replaceAll("in ", "");
   }
-}
-
-function handleHourClick(add = true) {
-  const currentValue = (document.getElementById("hoursInput").value = !add
-    ? parseInt(document.getElementById("hoursInput").value) - 1
-    : parseInt(document.getElementById("hoursInput").value) + 1);
-
-  setEndTime();
-}
-
-function handleMinuteClick(add = true) {
-  const currentValue = (document.getElementById("minutesInput").value = !add
-    ? parseInt(document.getElementById("minutesInput").value) - 1
-    : parseInt(document.getElementById("minutesInput").value) + 1);
-  setEndTime();
-}
-
-function handleSecondClick(add = true) {
-  const currentValue = (document.getElementById("secondsInput").value = !add
-    ? parseInt(document.getElementById("secondsInput").value) - 1
-    : parseInt(document.getElementById("secondsInput").value) + 1);
-  setEndTime();
 }
 
 function setCurrentTime() {
   const now = new Date();
   document.getElementById("currentTime").innerHTML = now.toLocaleTimeString();
-  if (!timeSet) {
-    setEndTime();
-  } else {
-    setRemainingTime();
-  }
+
+  setRemainingTime();
 }
 
 function startClick() {
@@ -88,3 +84,5 @@ function startClick() {
 setInterval(() => {
   setCurrentTime();
 }, 1000);
+
+setEndTime();
